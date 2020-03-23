2cc454e3a1d84035ce7b233aed3c520ac4e5d530
Ilia Choly <icholy@accipiterradar.com>
2020-03-23 13:07:31 -0400

    expand tests

--- rtsp_test.go
+++ rtsp_test.go
@@ -3,43 +3,59 @@ package rtsp
 import (
 	"bufio"
 	"bytes"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"gotest.tools/v3/assert"
 )
 
 func TestResponse(t *testing.T) {
-	res, err := NewResponse(StatusNotFound, []byte("Hello world"))
+	files, err := filepath.Glob("testdata/*.response")
 	assert.NilError(t, err)
-	res.Header["CSeq"] = "100"
-	res.Header["foo"] = "bar"
-
-	// encode it
-	var buf bytes.Buffer
-	err = res.Write(&buf)
-	assert.NilError(t, err)
-
-	// decode it
-	res2, err := ReadResponse(bufio.NewReader(&buf))
-	assert.NilError(t, err)
-	assert.DeepEqual(t, res, res2)
+	for _, file := range files {
+		t.Run(file, func(t *testing.T) {
+			// decode it
+			f, err := os.Open(file)
+			assert.NilError(t, err)
+			defer f.Close()
+			req, err := ReadResponse(bufio.NewReader(f))
+			assert.NilError(t, err)
+			// encode it
+			var buf bytes.Buffer
+			err = req.Write(&buf)
+			assert.NilError(t, err)
+			// decode it again
+			req2, err := ReadResponse(bufio.NewReader(&buf))
+			assert.NilError(t, err)
+			// compare to original
+			assert.DeepEqual(t, req, req2)
+		})
+	}
 }
 
 func TestRequest(t *testing.T) {
-	req, err := NewRequest(MethodOptions, "rtsp://someurl", []byte("what"))
+	files, err := filepath.Glob("testdata/*.request")
 	assert.NilError(t, err)
-	req.Header["CSeq"] = "1"
-	req.Header["Authorize"] = "secret"
-
-	// encode it
-	var buf bytes.Buffer
-	err = req.Write(&buf)
-	assert.NilError(t, err)
-
-	// decode it
-	req2, err := ReadRequest(bufio.NewReader(&buf))
-	assert.NilError(t, err)
-	assert.DeepEqual(t, req, req2)
+	for _, file := range files {
+		t.Run(file, func(t *testing.T) {
+			// decode it
+			f, err := os.Open(file)
+			assert.NilError(t, err)
+			defer f.Close()
+			req, err := ReadRequest(bufio.NewReader(f))
+			assert.NilError(t, err)
+			// encode it
+			var buf bytes.Buffer
+			err = req.Write(&buf)
+			assert.NilError(t, err)
+			// decode it again
+			req2, err := ReadRequest(bufio.NewReader(&buf))
+			assert.NilError(t, err)
+			// compare to original
+			assert.DeepEqual(t, req, req2)
+		})
+	}
 }
 
 func TestFrame(t *testing.T) {
