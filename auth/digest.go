53bc2cc068fbbb5e53f0ff4cbfc4e25207edf938
Ilia Choly <ilia.choly@gmail.com>
2020-06-11 13:39:31 -0400

    use icholy/digest

--- auth/digest.go
+++ auth/digest.go
@@ -1,34 +1,10 @@
-// Copyright 2013 M-Lab
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//
-// most of this code was taken from: https://github.com/bobziuchkovski/digest
 package auth
 
 import (
-	"crypto/md5"
-	"crypto/rand"
-	"errors"
-	"fmt"
-	"io"
-	"strings"
+	"net/http"
 
+	"github.com/icholy/digest"
 	"github.com/icholy/rtsp"
-)
-
-var (
-	ErrBadChallenge      = errors.New("Challenge is bad")
-	ErrAlgNotImplemented = errors.New("Alg not implemented")
 )
 
 // Digest is an Auth implementation for the digest authentication.
@@ -50,173 +26,19 @@ func (a Digest) Authorize(req *rtsp.Request, resp *rtsp.Response) (bool, error) 
 	if resp == nil {
 		return true, nil
 	}
-
-	c, err := a.getChallenge(resp.Header["WWW-Authenticate"])
+	chal, err := digest.FindChallenge(http.Header(resp.Header))
 	if err != nil {
 		return false, err
 	}
-
-	cr := &credentials{
-		Username:   a.Username,
-		Password:   a.Password,
-		Method:     req.Method,
-		Realm:      c.Realm,
-		Nonce:      c.Nonce,
-		DigestURI:  req.URL.RequestURI(),
-		Algorithm:  c.Algorithm,
-		Opaque:     c.Opaque,
-		MessageQop: c.Qop, // "auth" must be a single value
-		NonceCount: 0,
-	}
-
-	auth, err := cr.authorize()
+	cred, err := digest.Digest(chal, digest.Options{
+		Method:   req.Method,
+		URI:      req.URL.RequestURI(),
+		Username: a.Username,
+		Password: a.Password,
+	})
 	if err != nil {
 		return false, err
 	}
-
-	// Make authenticated request.
-	req.Header.Set("Authorization", auth)
+	req.Header.Set("Authorization", cred.String())
 	return true, nil
-}
-
-func (a Digest) getChallenge(values []string) (*challenge, error) {
-	for _, v := range values {
-		c, err := parseChallenge(v)
-		if err == nil {
-			return c, nil
-		}
-	}
-	return nil, ErrBadChallenge
-}
-
-type challenge struct {
-	Realm     string
-	Domain    string
-	Nonce     string
-	Opaque    string
-	Stale     string
-	Algorithm string
-	Qop       string
-}
-
-func parseChallenge(input string) (*challenge, error) {
-	const ws = " \n\r\t"
-	const qs = `"`
-	s := strings.Trim(input, ws)
-	if !strings.HasPrefix(s, "Digest ") {
-		return nil, ErrBadChallenge
-	}
-	s = strings.Trim(s[7:], ws)
-	sl := strings.Split(s, ", ")
-	c := &challenge{
-		Algorithm: "MD5",
-	}
-	var r []string
-	for i := range sl {
-		r = strings.SplitN(sl[i], "=", 2)
-		switch r[0] {
-		case "realm":
-			c.Realm = strings.Trim(r[1], qs)
-		case "domain":
-			c.Domain = strings.Trim(r[1], qs)
-		case "nonce":
-			c.Nonce = strings.Trim(r[1], qs)
-		case "opaque":
-			c.Opaque = strings.Trim(r[1], qs)
-		case "stale":
-			c.Stale = strings.Trim(r[1], qs)
-		case "algorithm":
-			c.Algorithm = strings.Trim(r[1], qs)
-		case "qop":
-			//TODO(gavaletz) should be an array of strings?
-			c.Qop = strings.Trim(r[1], qs)
-		default:
-			return nil, ErrBadChallenge
-		}
-	}
-	return c, nil
-}
-
-type credentials struct {
-	Username   string
-	Password   string
-	Method     string
-	DigestURI  string
-	Realm      string
-	Nonce      string
-	Algorithm  string
-	Cnonce     string
-	Opaque     string
-	MessageQop string
-	NonceCount int
-}
-
-func h(data string) string {
-	hf := md5.New()
-	io.WriteString(hf, data)
-	return fmt.Sprintf("%x", hf.Sum(nil))
-}
-
-func kd(secret, data string) string {
-	return h(fmt.Sprintf("%s:%s", secret, data))
-}
-
-func (c *credentials) ha1() string {
-	return h(fmt.Sprintf("%s:%s:%s", c.Username, c.Realm, c.Password))
-}
-
-func (c *credentials) ha2() string {
-	return h(fmt.Sprintf("%s:%s", c.Method, c.DigestURI))
-}
-
-func (c *credentials) resp(cnonce string) (string, error) {
-	c.NonceCount++
-	if c.MessageQop == "auth" {
-		if cnonce != "" {
-			c.Cnonce = cnonce
-		} else {
-			b := make([]byte, 8)
-			io.ReadFull(rand.Reader, b)
-			c.Cnonce = fmt.Sprintf("%x", b)[:16]
-		}
-		return kd(c.ha1(), fmt.Sprintf("%s:%08x:%s:%s:%s",
-			c.Nonce, c.NonceCount, c.Cnonce, c.MessageQop, c.ha2())), nil
-	} else if c.MessageQop == "" {
-		return kd(c.ha1(), fmt.Sprintf("%s:%s", c.Nonce, c.ha2())), nil
-	}
-	return "", ErrAlgNotImplemented
-}
-
-func (c *credentials) authorize() (string, error) {
-	// Note that this is only implemented for MD5 and NOT MD5-sess.
-	// MD5-sess is rarely supported and those that do are a big mess.
-	if c.Algorithm != "MD5" {
-		return "", ErrAlgNotImplemented
-	}
-	// Note that this is NOT implemented for "qop=auth-int".  Similarly the
-	// auth-int server side implementations that do exist are a mess.
-	if c.MessageQop != "auth" && c.MessageQop != "" {
-		return "", ErrAlgNotImplemented
-	}
-	resp, err := c.resp("")
-	if err != nil {
-		return "", ErrAlgNotImplemented
-	}
-	sl := []string{fmt.Sprintf(`username="%s"`, c.Username)}
-	sl = append(sl, fmt.Sprintf(`realm="%s"`, c.Realm))
-	sl = append(sl, fmt.Sprintf(`nonce="%s"`, c.Nonce))
-	sl = append(sl, fmt.Sprintf(`uri="%s"`, c.DigestURI))
-	sl = append(sl, fmt.Sprintf(`response="%s"`, resp))
-	if c.Algorithm != "" {
-		sl = append(sl, fmt.Sprintf(`algorithm="%s"`, c.Algorithm))
-	}
-	if c.Opaque != "" {
-		sl = append(sl, fmt.Sprintf(`opaque="%s"`, c.Opaque))
-	}
-	if c.MessageQop != "" {
-		sl = append(sl, fmt.Sprintf("qop=%s", c.MessageQop))
-		sl = append(sl, fmt.Sprintf("nc=%08x", c.NonceCount))
-		sl = append(sl, fmt.Sprintf(`cnonce="%s"`, c.Cnonce))
-	}
-	return fmt.Sprintf("Digest %s", strings.Join(sl, ", ")), nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/icholy/rtsp
 
 go 1.14
 
-require gotest.tools/v3 v3.0.2
+require (
+	github.com/icholy/digest v0.1.7
+	gotest.tools/v3 v3.0.2
+)
